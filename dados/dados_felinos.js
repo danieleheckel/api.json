e0ff860e2eea1b0007b5c52f1ee846db6dc3ff35
Amayya <105234629+danieleheckel@users.noreply.github.com>
2024-10-24 08:44:35 -0300

    Delete dados directory

--- dados/dados_felinos.js
+++
@@ -1,6 +0,0 @@
-{
-    "Quantidade_de_gatos_no_Brasil_(milhoes)": 41.6 ,
-    "Quantidade_de_gatos_no_mundo_(milhoes)": 500 ,
-    "Quantidade_de_gatos_de_rua_no_Brasil_(milhoes)": 200 ,
-    "Quantidade_de_gatos_de_rua_no_mundo_(milhoes)": 10 ;
-}
