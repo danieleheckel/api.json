d35a5f90da8ce9fec513ac22a01353f5ee2d4d9a
Amayya <105234629+danieleheckel@users.noreply.github.com>
2024-10-24 08:00:51 -0300

    Add files via upload

---
+++ dados_felinos.js
@@ -0,0 +1,6 @@
+{
+    "Quantidade_de_gatos_no_Brasil_(milhões)": 41.6 ,
+    "Quantidade_de_gatos_no_mundo_(milhões)": 500 ,
+    "Quantidade_de_gatos_de_rua_no_Brasil_(milhões)": 200 ,
+    "Quantidade_de_gatos_de_rua_no_mundo_(milhões)": 10 ;
+}
